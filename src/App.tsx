44cb950b651f1e038be099bc30609ac8d8bb2026
github Junichi-Machii <junichi878338@hotmail.co.jp>
2023-05-15 14:41:36 +0900

    app complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
+// import React from 'react';
+import { useState } from "react";
+
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Results from "./components/Results";
+
+import "./App.css";
+
+type ResultsStateType = {
+  country: string,
+  cityName: string,
+  temperature: string,
+  conditionText: string,
+  icon: string,
+}
+
+
 
 function App() {
+  const [ city, setCity ]= useState<string>();
+
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  })
+  const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      fetch(`http://api.weatherapi.com/v1/current.json?key=c1c9c437aa154a06b2b14007231505&q=${city}&aqi=no`)
+        .then(res => res.json() )
+        .then(data => {
+          setResults({
+            country: data.location.country,
+            cityName: data.location.name,
+            temperature: data.current.temp_c,
+            conditionText: data.current.condition.text,
+            icon: data.current.condition.icon,
+          })
+        })
+
+  }
+  // console.log(results)
   return (
-    <div className="App">
-      <h1>HELLO</h1>
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
   );
 }
